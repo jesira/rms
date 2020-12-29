555cea98942d906358ccc990525fe9511b80bae3
jesira <jesiraruhi@yahoo.com>
2020-12-29 23:48:01 +0600

    updated model and added total mark

--- result/models.py
+++ result/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
+
 # Create your models here.
+
+class ResultSubmission(models.Model):
+    STUDENT_ID = (
+        ('201132', '201132'),
+        ('301132', '301132'),
+        ('401132', '401132'),
+        ('501132', '501132'),
+        ('601132', '601132'),
+    )
+
+    COURSE_ID = (
+        ('IIT01', 'IIT01'),
+        ('IIT02', 'IIT02'),
+        ('IIT03', 'IIT03'),
+    )
+    student_id = models.CharField(max_length=200, null=True, choices=STUDENT_ID)
+    course_id = models.CharField(max_length=200, null=True, choices=COURSE_ID)
+    class_test_mark = models.FloatField(null=True)
+    final_mark = models.FloatField(null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.student_id
+
+
--- result/views.py
+++ result/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
+from django.db.models import F
 
 
 def home(request):
-    return render(request, 'result/home.html')
+    results = ResultSubmission.objects.all().annotate(total_mark=F('class_test_mark') + F('final_mark'))
+    current_user = request.user
+    teacher_id = current_user.id
+    context = {'results': results, 'teacher_id': teacher_id}
+    return render(request, 'result/home.html', context)
+
+
